cc4b9389aa41912d59c4799455bd4e49ef2a2533
kevim <k.v.n.such@gmail.com>
2021-05-23 15:45:13 -0300

    Configzinha marota

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,19 @@
 import { Greeter } from './greeter';
+import FingerprintJS from '@fingerprintjs/fingerprintjs';
+
 
 const g = new Greeter('Juri');
 g.greet();
+
+
+
+const fpPromise = FingerprintJS.load();
+(async () => {
+    // Get the visitor identifier when you need it.
+    const fp = await fpPromise;
+    const result = await fp.get();
+
+    // This is the visitor identifier:
+    const visitorId = result.visitorId;
+    console.log(visitorId);
+})();
